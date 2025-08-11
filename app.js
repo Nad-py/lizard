b8f00e9d55fd75763750e0b9ca9dbf0dd8453c22
Nad-py <naduses@gmail.com>
2025-08-11 06:35:51 +0200

    fixed silly mistakes ^^

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const PARAMS = new URLSearchParams(window.location.search);
 const LIZARDSOUND = document.getElementById("lizardSound");
 const USERNAME = PARAMS.get("username");
 const VOLUME = parseInt(PARAMS.get("volume"));
-const MODPERMISSION = (PARAMS.get("modpermission") ?? "true") === "true";
+const MODPERMISSION = (PARAMS.get("modpermission") ?? "true").toLowerCase() === "true";
 let appIsOn = true;
 
 ComfyJS.onChat = ( user, message, flags, self, extra ) => {
     const isAllowedToChange = flags["broadcaster"] || (MODPERMISSION == true && flags["mod"]);
     const msgLower = message.toLowerCase();
 
-    if(msgLower == "stoplizarding" && isAllowedToChange){
+    if(isAllowedToChange && msgLower == "stoplizarding"){
         appIsOn = false;
     }
-    if(msgLower == "startlizarding" && isAllowedToChange){
+    if(isAllowedToChange && msgLower == "startlizarding"){
         appIsOn = true;
     }
 
-    if(msgLower.includes("lizard") && appIsOn){
+    if(appIsOn && msgLower.includes("lizard") ){
         let normalizedVolume = !isNaN(VOLUME) ? Math.max(0.01, Math.min(1, VOLUME / 100)) : 1;
         LIZARDSOUND.volume = normalizedVolume; 
-        LIZARDSOUND.currentTime = 0;
+        LIZARDSOUND.currentTime = 0;    
         LIZARDSOUND.play();
     }
 
